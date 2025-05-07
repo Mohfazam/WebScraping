88f52c69d0d366784748188da4759539ba6a4840
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-07 14:08:13 +0530

    Books To Scrap

--- Second/main.py
+++ Second/main.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://m.timesjobs.com/mobile/jobs-search-result.html?txtKeywords=python&cboWorkExp1=-1&txtLocation=').text
+html_text = requests.get('https://books.toscrape.com/').text
 soup = BeautifulSoup(html_text, 'lxml')
-print(soup)
+books  = soup.find_all('li', class_='col-xs-6 col-sm-4 col-md-3 col-lg-3')
+
+for book in books:
+    title = book.h3.a['title']
+    print(title)
