6c41f1abd9096a57afc3730890400882453674c0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-07 12:59:41 +0530

    Tags formatting

--- First/main.py
+++ First/main.py
@@ -5,4 +5,5 @@ with open(r"C:/Azam/WebScraping/First/index.html", 'r') as html_file:
 
     soup = BeautifulSoup(content, 'lxml')
     tags = soup.find_all('h1')
-    print(tags)
+    for tag in tags: 
+        print(tag.text)
