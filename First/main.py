04d2d7a1839190f90dc1ef49f92591c3691fd934
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-07 12:56:17 +0530

    Tags 3

--- First/main.py
+++ First/main.py
@@ -4,5 +4,5 @@ with open(r"C:/Azam/WebScraping/First/index.html", 'r') as html_file:
     content  = html_file.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    tags = soup.find('h1')
+    tags = soup.find_all('h1')
     print(tags)
